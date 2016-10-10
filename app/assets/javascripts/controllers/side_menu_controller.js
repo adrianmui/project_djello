e9dba4dbd8d19652debe22701f39f667f577209d
Adrian Mui <adrian.mui@gmail.com>
2016-10-09 19:15:35 -0500

    boards working, select dropdown working, ok

--- app/assets/javascripts/controllers/side_menu_controller.js
+++ app/assets/javascripts/controllers/side_menu_controller.js
@@ -6,10 +6,11 @@
 // 4. (opt) show newsfeed/activity
 
 app.controller('SideMenuCtrl',
-  ['$scope', 'currUser', 'boards', 'BoardService',
-  function($scope, currUser, boards, BoardService) {
+  ['$scope', 'currUser', 'boards', 'BoardService', '$state',
+  function($scope, currUser, boards, BoardService, $state) {
   
   $scope.boardParams = {};
+  $scope.selectedBoard;
 
   console.log('you are in Side Menu controller');
   console.log('boards: ', boards);
@@ -28,6 +29,11 @@ app.controller('SideMenuCtrl',
     };
     BoardService.create($scope.boardParams, currUser.id);
   };
+
+  $scope.changeView = function(){
+    $state.go('main.boards.show', {id: parseInt($scope.selectedBoard.id)});
+    // $state.go('main.boards.show({id:' + parseInt($scope.selectedBoard.id) + '})'); // path not hash
+  }
 
   
 
